23a17aefbd96ca43d5a17e71003af186f464d039
Zeti_1223 <121749710+zeti1223@users.noreply.github.com>
2026-09-06 21:32:01 +0200

    little fix

--- src/content.js
+++ src/content.js
@@ -269,7 +269,7 @@ export const privacyPolicy = {
   sections: [
     {
       heading: '1. Az Adatkezelőre vonatkozó információk',
-      body: `Név: André Melinda egyéni vállalkozó\nSzékhely: 2030 Érd Betonozó utca 21. \nTelephely: 2030 Érd Betonozó utca 21.\nKépviseli: André Melinda\nNyilvántartási szám: 62746883\nBejegyző hatóság neve: Pest Vármegyei és Érdi Kereskedelmi és Iparkamara\nSzámlavezető bank neve: MBH Bank Nyrt.\nAdószám: 92328583-1-33\nEmail címe: kapcsolat@skipintro.hu\nHonlap: www.skipintro.hu\nAdatvédelmi tisztviselő neve: a GDPR 37. cikk szerint nem alkalmaz.`,
+      body: `Név: André Melinda egyéni vállalkozó\nSzékhely: 2030 Érd Betonozó utca 21.\nKépviseli: André Melinda\nNyilvántartási szám: 62746883\nBejegyző hatóság neve: Pest Vármegyei és Érdi Kereskedelmi és Iparkamara\nSzámlavezető bank neve: MBH Bank Nyrt.\nAdószám: 92328583-1-33\nEmail címe: kapcsolat@skipintro.hu\nHonlap: www.skipintro.hu\nAdatvédelmi tisztviselő neve: a GDPR 37. cikk szerint nem alkalmaz.`,
     },
     {
       heading: '2. Milyen adatokat kezelek?',
